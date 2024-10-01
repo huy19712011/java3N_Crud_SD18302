acb66d4dfd0555bd86ab62fedee9708e50c48ec3
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-10-01 13:24:15 +0700

    JDBC: connection; create table, insert record

---
+++ src/main/java/com/example/java3n_crud_sd18302/jdbc/TestConnection.java
@@ -0,0 +1,19 @@
+package com.example.java3n_crud_sd18302.jdbc;
+
+public class TestConnection {
+
+    public static void main(String[] args) {
+
+        DatabaseConnectionManager dcm =
+                new DatabaseConnectionManager("test1", "sa", "123456");
+
+        try {
+
+            dcm.getConnection();
+            System.out.println("Connected...");
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+}
